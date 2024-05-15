3f2d94d8931b1f5f5e3ec52f18ba8bf01727f2ec
Lopezandres07 <andlop17@gmail.com>
2024-05-15 13:56:27 -0400

    This reverts commit 63fba58a65699bc3c17d68bca3efca886a26c328.

--- frontEnd/src/views/Opinions.jsx
+++ frontEnd/src/views/Opinions.jsx
@@ -6,24 +6,24 @@ export const Opinions = () => {
 
   return (
     <>
+      <h3>¡Envíanos tu opinión o ideas de mejora!</h3>
       <div className='opinion-content'>
         <form>
-          <h3>¡Envíanos tu opinión o ideas de mejora!</h3>
           <p>
             En este apartado, pueden compartir sus comentarios, sugerencias e
             ideas de mejora sobre la plataforma. A través de este, se busca
             fomentar la participación activa de la comunidad para recopilar
             información valiosa que nos ayude a entender sus necesidades y
             expectativas.
-          </p>
-          <p>
-            El equipo puede compartir sus opiniones sobre la usabilidad del
-            sitio, las funcionalidades deseadas, los problemas encontrados y
-            cualquier otra sugerencia relevante. Esto nos permite mejorar
-            continuamente la experiencia del usuario y ofrecer un servicio más
-            adaptado y satisfactorio. Los comentarios enviados a través de este
-            componente serán revisados por nuestro equipo para evaluar posibles
-            mejoras y ajustes en la plataforma.
+            <p>
+              El equipo puede compartir sus opiniones sobre la usabilidad del
+              sitio, las funcionalidades deseadas, los problemas encontrados y
+              cualquier otra sugerencia relevante. Esto nos permite mejorar
+              continuamente la experiencia del usuario y ofrecer un servicio más
+              adaptado y satisfactorio. Los comentarios enviados a través de
+              este componente serán revisados por nuestro equipo para evaluar
+              posibles mejoras y ajustes en la plataforma.
+            </p>
           </p>
           <label htmlFor='email'>Tu correo electrónico:</label>
           <input
