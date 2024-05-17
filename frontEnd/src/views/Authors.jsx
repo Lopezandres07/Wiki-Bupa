fe251b35b0be97a047abbce190376ecbc9b1aa1d
Lopezandres07 <andlop17@gmail.com>
2024-05-17 14:52:38 -0400

    Adding authors content

--- frontEnd/src/views/Authors.jsx
+++ frontEnd/src/views/Authors.jsx
@@ -1,5 +1,48 @@
 import React from 'react'
 
 export const Authors = () => {
-  return <div>Authors</div>
+  return (
+    <div className='authors-content'>
+      <div>
+        <h3>Iniciativa del Repositorio</h3>
+        <p>
+          En vista que, la carpeta Pasolini que se encuentra en red se piensa
+          dar de baja, la información existente relacionada a procedimientos,
+          aplicativos y demás que es de suma importancia para el equipo de
+          Soporte TI en el desarrollo de sus funciones, puede verse comprometida
+          por lo mencionado anteriormente, es por ello que, se ve la posibilidad
+          de elaborar un repositorio web para alojar todas las herramientas del
+          equipo TI y que estén disponibles para ellos al momento de que se haga
+          la migración respectiva.
+        </p>
+      </div>
+      <div className='authors-content-img'>
+        <img
+          src='/img/Soporte-TI-1.png'
+          alt='Authors Photo'
+        />
+        <div>
+          <h3>Autores del Blog</h3>
+          <p>
+            La iniciativa de este proyecto, va de la mano de Jaime Méndez y
+            Andrés López, quienes se plantearon la posibilidad de llevar esta
+            idea a un repositorio web, mediante programación, elaborando el
+            portal y adaptándolo con los nombres de las áreas del negocio, y
+            organizando la información en cada una de ellas mediante categorias
+            para así tener todo más detallado y al alance de quien lo necesite.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h3>Comunidad TI</h3>
+        <p>
+          Así como, los administradores del repositorio ya mencionados cargarán
+          la información ya creada en el Pasolini, el equipo de Soporte TI forma
+          un papel muy importante en nutrir el blog, con contenido asociado que
+          sirva como apoyo para el departamento, y con ello tener a la mano
+          cualquier detalle que facilite las labores de grupo.
+        </p>
+      </div>
+    </div>
+  )
 }
