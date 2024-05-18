8d17b2a9ee9bd3a2c58817496d62dbc96dc698ce
Lopezandres07 <andlop17@gmail.com>
2024-05-18 16:22:41 -0400

    Changed home position and CSS styles

--- frontEnd/src/views/Authors.jsx
+++ frontEnd/src/views/Authors.jsx
@@ -15,25 +15,10 @@ export const Authors = () => {
           equipo TI y que estén disponibles para ellos al momento de que se haga
           la migración respectiva.
         </p>
-      </div>
-      <div className='authors-content-img'>
         <img
           src='/img/Soporte-TI-1.png'
           alt='Authors Photo'
         />
-        <div>
-          <h3>Autores del Blog</h3>
-          <p>
-            La iniciativa de este proyecto, va de la mano de Jaime Méndez y
-            Andrés López, quienes se plantearon la posibilidad de llevar esta
-            idea a un repositorio web, mediante programación, elaborando el
-            portal y adaptándolo con los nombres de las áreas del negocio, y
-            organizando la información en cada una de ellas mediante categorias
-            para así tener todo más detallado y al alance de quien lo necesite.
-          </p>
-        </div>
-      </div>
-      <div>
         <h3>Comunidad TI</h3>
         <p>
           Así como, los administradores del repositorio ya mencionados cargarán
@@ -42,6 +27,25 @@ export const Authors = () => {
           sirva como apoyo para el departamento, y con ello tener a la mano
           cualquier detalle que facilite las labores de grupo.
         </p>
+      </div>
+      <div>
+        <img
+          src='/img/idea.png'
+          alt='Idea Photo'
+        />
+        <h3>Autores del Blog</h3>
+        <p>
+          La iniciativa de este proyecto, va de la mano de Jaime Méndez y Andrés
+          López, quienes se plantearon la posibilidad de llevar esta idea a un
+          repositorio web, mediante programación, elaborando el portal y
+          adaptándolo con los nombres de las áreas del negocio, y organizando la
+          información en cada una de ellas mediante categorias para así tener
+          todo más detallado y al alance de quien lo necesite.
+        </p>
+        <img
+          src='/img/support-team.png'
+          alt='Support Team Photo'
+        />
       </div>
     </div>
   )
--- frontEnd/src/views/Home.jsx
+++ frontEnd/src/views/Home.jsx
@@ -3,54 +3,56 @@ import React from 'react'
 export const Home = () => {
   return (
     <>
-      <div className='home-title'>
+      <article className='home-content'>
         <h2>Bienvenidos a la Wiki Bupa</h2>
         <h3>
           ¡Tu fuente de conocimiento en las distintas áreas de la empresa!
         </h3>
-      </div>
-      <article className='home-content'>
-        <div>
-          <p>
-            Desplázate por la barra lateral donde podrás visualizar en las
-            diferentes opciones:
-          </p>
-          <ul>
-            <li>
-              Manuales: Acá se almacenan los procedimientos de instalación de
-              aplicativos, así como sus instaladores y demás.
-            </li>
-            <li>
-              Errores y Soluciones: En este apartado se muestran tips para
-              solventar problemas recurrentes de los aplicativos.
-            </li>
-            <li>
-              Aplicaciones: Mediante esta opción, obtendras aplicativos que son
-              útiles para las tareas que desempeñen los usuarios.
-            </li>
-          </ul>
-          <p>
-            De igual manera, tienes disponible la barra de busqueda en la parte
-            superior izquierda de la página, donde podrás buscar algún tema en
-            específico.
-          </p>
-          <img
-            src='/img/ti.png'
-            alt='home-bg'
-          />
-        </div>
-        <div>
-          <p>
-            Si tienes algún tema referente sobre algún procedimiento, tips nuevo
-            aplicativo que gustes aportar para apoyar a la comunidad, puedes
-            iniciar sesión con tu cuenta Bupa para que tengas habilitada la
-            opción <b>"Crear Publicación"</b> en la barra de navegación.
-          </p>
+        <div className='home-content-center'>
+          <div>
+            <p>
+              Desplázate por la barra lateral donde podrás visualizar en las
+              diferentes opciones:
+            </p>
+            <ul>
+              <li>
+                Manuales: Acá se almacenan los procedimientos de instalación de
+                aplicativos, así como sus instaladores y demás.
+              </li>
+              <li>
+                Errores y Soluciones: En este apartado se muestran tips para
+                solventar problemas recurrentes de los aplicativos.
+              </li>
+              <li>
+                Aplicaciones: Mediante esta opción, obtendras aplicativos que
+                son útiles para las tareas que desempeñen los usuarios.
+              </li>
+            </ul>
+          </div>
           <img
             src='/img/home-bg.png'
             alt='home-bg'
           />
+          <div>
+            <p>
+              Si tienes algún tema referente sobre algún procedimiento, tips
+              nuevo aplicativo que gustes aportar para apoyar a la comunidad,
+              puedes iniciar sesión con tu cuenta Bupa para que tengas
+              habilitada la opción <b>"Crear Publicación"</b> en la barra de
+              navegación.
+            </p>
+            <p>
+              En ella, podrás crear una publicación con el tema que desees,
+              indicándole una categroría asociada y el área a la que pertenece,
+              además de asociar los archivos necesarios según el tema.
+            </p>
+          </div>
         </div>
+        <p>
+          De igual manera, tienes disponible la barra de busqueda en la parte
+          superior izquierda de la página, donde podrás realizar busquedas de
+          algún tema en específico.
+        </p>
       </article>
     </>
   )
